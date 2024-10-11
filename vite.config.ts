e1db65c8cf39951d1dacadd3e12bd35973d21468
Sammie Kei <58369899+SammieKei40@users.noreply.github.com>
2024-10-11 08:25:14 +0100

    fixed deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import vue from "@vitejs/plugin-vue";
 import path from "path";
-import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from "vite";
 // import eslintPlugin from 'vite-plugin-eslint';
 

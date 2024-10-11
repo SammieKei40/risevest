625130b17f658e975380a25915441db0519655c7
Sammie Kei <58369899+SammieKei40@users.noreply.github.com>
2024-10-11 12:31:46 +0100

    fixed carousel testimonial

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,8 @@ import { createApp } from 'vue'
 import './assets/css/main.scss'
 import App from './App.vue'
 import router from './router'
-import VueCarousel from '@chenfengyuan/vue-carousel';
 
 const app = createApp(App)
 
-app.component(VueCarousel.name, VueCarousel);
 app.use(router)
 app.mount('#app')
